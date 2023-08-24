e1f3fa043d91d6ee10d88cd975f541d3e02e4c9e
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-08-24 19:04:35 +0900

    project init

---
+++ src/main/java/com/jdh/hoxy_api/HoxyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdh.hoxy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoxyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoxyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdh/hoxy_api/HoxyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdh.hoxy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoxyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
