5ea112c121c4a705b3e0d16eb3af881382ba3fd1
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-08-27 00:44:22 +0900

    feat(StoreGetResponseDTO.java): StoreGetResponseDTO 추가

---
+++ src/main/java/com/jdh/hoxy_api/api/store/dto/StoreGetResponseDTO.java
@@ -0,0 +1,21 @@
+package com.jdh.hoxy_api.api.store.dto;
+
+import com.jdh.hoxy_api.api.store.domain.entity.Store;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class StoreGetResponseDTO {
+
+    private final String name;
+
+    public static StoreGetResponseDTO of(Store store) {
+        return StoreGetResponseDTO.builder()
+                .name(store.getName())
+                .build();
+    }
+
+}
