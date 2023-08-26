986c3a7ca67f119f9735a34a679de6e6b03f52fb
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-08-27 01:19:20 +0900

    test: StoreControllerTest 업체 조회 test

---
+++ src/test/java/com/jdh/hoxy_api/api/store/controller/StoreControllerTest.java
@@ -0,0 +1,78 @@
+package com.jdh.hoxy_api.api.store.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.jdh.hoxy_api.api.store.application.impl.StoreGetServiceImpl;
+import com.jdh.hoxy_api.api.store.domain.entity.Store;
+import com.jdh.hoxy_api.api.store.dto.StoreGetResponseDTO;
+import com.jdh.hoxy_api.config.security.TestSecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {StoreController.class, ObjectMapper.class})
+@WebAppConfiguration
+@EnableWebMvc
+@AutoConfigureMockMvc
+@Import({TestSecurityConfig.class})
+public class StoreControllerTest {
+
+    @MockBean
+    StoreGetServiceImpl storeGetService;
+
+    @Autowired
+    private MockMvc mockMvc;
+    private Gson gson;
+
+    @BeforeEach
+    public void init() {
+        gson = new Gson();
+    }
+
+    @Test
+    public void Store_조회_성공() throws Exception {
+        // given
+        final StoreGetResponseDTO store = StoreGetResponseDTO.builder()
+                .name("테스트")
+                .build();
+        final List<StoreGetResponseDTO> resultList = new ArrayList<>(){{
+            add(store);
+        }};
+        when(storeGetService.getStoreList()).thenReturn(resultList);
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get("/store")
+        );
+
+        // then
+        resultActions.andExpect(status().isOk());
+
+        final List<StoreGetResponseDTO> response = gson.fromJson(resultActions.andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8), new TypeToken<List<StoreGetResponseDTO>>(){}.getType());
+        assertThat(response.get(0).getName()).isEqualTo("테스트");
+    }
+
+}
