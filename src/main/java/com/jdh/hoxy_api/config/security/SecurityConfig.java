6e33bd84bb1541cc2fb173c9094ef96f40c487fa
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-08-27 01:17:02 +0900

    feat(SecurityConfig.java): Spring Security Config 추가

---
+++ src/main/java/com/jdh/hoxy_api/config/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.jdh.hoxy_api.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Bean
+    public SecurityFilterChain config(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.headers((headers) ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(mvc.pattern("/store")).permitAll());
+
+        return http.build();
+    }
+
+}
