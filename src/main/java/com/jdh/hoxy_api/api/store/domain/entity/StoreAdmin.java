e9b846ee547f173a3b2959d149ad19713173ba1b
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-08-28 22:33:12 +0900

    feat(StoreAdmin.java): StoreAdmin Entity 암호화 관련 기능 추가
    1. 비밀번호 암호화 추가
    2. 비밀번호 체크 추가
    3. BCrypt 적용으로 인한 salt 제거

--- src/main/java/com/jdh/hoxy_api/api/store/domain/entity/StoreAdmin.java
+++ src/main/java/com/jdh/hoxy_api/api/store/domain/entity/StoreAdmin.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Getter
@@ -20,8 +21,6 @@ public class StoreAdmin extends RegModDtEntity {
 
     private String password;
 
-    private String pwSalt;
-
     private String name;
 
     @OneToOne
@@ -32,11 +31,28 @@ public class StoreAdmin extends RegModDtEntity {
         this.store = store;
     }
 
+    /**
+     * 패스워드 암호화
+     * @param passwordEncoder 암호화에 사용할 인코더 클래스
+     */
+    public void encryptPassword(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+
+    /**
+     * 패스워드 일치 여부 확인
+     * @param plainPassword 입력받은 암호화 이전의 비밀번호
+     * @param passwordEncoder 암호화에 사용할 인코더 클래스
+     * @return 패스워드 일치 여부
+     */
+    public boolean checkPassword(String plainPassword, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(plainPassword, this.password);
+    }
+
     @Builder
-    protected StoreAdmin(String id, String password, String pwSalt, String name) {
+    protected StoreAdmin(String id, String password, String name) {
         this.id = id;
         this.password = password;
-        this.pwSalt = pwSalt;
         this.name = name;
     }
 
