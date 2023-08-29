6102041af2a2eefe973323e2a31578287178902a
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-08-29 21:16:26 +0900

    chore: StoreAdminException 에러코드 추가

--- src/main/java/com/jdh/hoxy_api/api/store/exception/enums/StoreAdminErrorResult.java
+++ src/main/java/com/jdh/hoxy_api/api/store/exception/enums/StoreAdminErrorResult.java
@@ -8,11 +8,12 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum StoreAdminErrorResult {
 
-    STORE_NOT_EXIST(HttpStatus.BAD_REQUEST, "존재하지 않는 업체입니다."),
-    DUPLICATE_STORE(HttpStatus.BAD_REQUEST, "이미 가입한 업체입니다."),
-    DUPLICATE_STORE_ADMIN_ID(HttpStatus.BAD_REQUEST, "중복되는 사용자 ID 입니다.");
+    STORE_NOT_EXIST(HttpStatus.BAD_REQUEST, "SA0001", "존재하지 않는 업체입니다."),
+    DUPLICATE_STORE(HttpStatus.BAD_REQUEST, "SA0002", "이미 가입한 업체입니다."),
+    DUPLICATE_STORE_ADMIN_ID(HttpStatus.BAD_REQUEST, "SA0003", "중복되는 사용자 ID 입니다.");
 
     private final HttpStatus status;
+    private final String code;
     private final String message;
 
 }
