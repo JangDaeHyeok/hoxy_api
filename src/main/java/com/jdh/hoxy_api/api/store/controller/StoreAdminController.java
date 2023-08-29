f74b5a6fbab90b8a2c47b320824db710fd2be991
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-08-29 21:21:00 +0900

    feat(StoreAdminController.java): StoreAdminController 추가
    1. 업체 관리자 등록 api 추가

---
+++ src/main/java/com/jdh/hoxy_api/api/store/controller/StoreAdminController.java
@@ -0,0 +1,26 @@
+package com.jdh.hoxy_api.api.store.controller;
+
+import com.jdh.hoxy_api.api.common.response.entity.ApiResponseEntity;
+import com.jdh.hoxy_api.api.store.application.StoreAdminAddService;
+import com.jdh.hoxy_api.api.store.dto.request.StoreAdminAddRequestDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class StoreAdminController {
+
+    private final StoreAdminAddService storeAdminAddService;
+
+    @PostMapping(value = "/store/admin")
+    public ResponseEntity<ApiResponseEntity> storeListGetApi(@RequestBody @Valid StoreAdminAddRequestDTO reqDTO) {
+        storeAdminAddService.addStoreAdmin(reqDTO.getStoreIdx(), reqDTO.getId(), reqDTO.getPassword(), reqDTO.getName());
+
+        return ApiResponseEntity.successResponseEntity();
+    }
+
+}
