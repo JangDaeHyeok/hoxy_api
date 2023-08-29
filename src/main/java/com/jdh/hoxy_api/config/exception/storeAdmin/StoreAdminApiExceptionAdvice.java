2fda9957ec5bcf47d7e93171e24062a937bb4b89
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-08-29 21:19:20 +0900

    feat(StoreAdminApiExceptionAdvice.java): StoreAdminException advice 추가

---
+++ src/main/java/com/jdh/hoxy_api/config/exception/storeAdmin/StoreAdminApiExceptionAdvice.java
@@ -0,0 +1,24 @@
+package com.jdh.hoxy_api.config.exception.storeAdmin;
+
+import com.jdh.hoxy_api.api.store.exception.StoreAdminException;
+import com.jdh.hoxy_api.config.exception.common.ApiExceptionEntity;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class StoreAdminApiExceptionAdvice {
+
+    @ExceptionHandler({StoreAdminException.class})
+    public ResponseEntity<ApiExceptionEntity> exceptionHandler(HttpServletRequest req, final StoreAdminException e) {
+        // e.printStackTrace();
+        return ResponseEntity
+                .status(e.getStoreAdminErrorResult().getStatus())
+                .body(ApiExceptionEntity.builder()
+                        .errorCode(e.getStoreAdminErrorResult().getCode())
+                        .errorMsg(e.getStoreAdminErrorResult().getMessage())
+                        .build());
+    }
+
+}
