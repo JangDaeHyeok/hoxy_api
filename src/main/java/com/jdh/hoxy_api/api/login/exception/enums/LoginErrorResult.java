ff9f3c94da3f89a6a4e73eea9c3ebadc575a2dda
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-08-30 21:53:20 +0900

    chore: LoginException 메시지 문구 수정

--- src/main/java/com/jdh/hoxy_api/api/login/exception/enums/LoginErrorResult.java
+++ src/main/java/com/jdh/hoxy_api/api/login/exception/enums/LoginErrorResult.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum LoginErrorResult {
 
-    ID_PASSWORD_NOT_CORRECT(HttpStatus.BAD_REQUEST, "L0001", "아이디나 패스워드가 올바르지 않습니다.");
+    ID_PASSWORD_NOT_CORRECT(HttpStatus.BAD_REQUEST, "L0001", "아이디나 비밀번호가 올바르지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
