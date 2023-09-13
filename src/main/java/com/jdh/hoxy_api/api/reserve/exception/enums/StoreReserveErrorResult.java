bf660727f3988bf973cbc20351b870d00c31a0bd
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-09-13 21:58:21 +0900

    feat: StoreReserve Error msg 수정

--- src/main/java/com/jdh/hoxy_api/api/reserve/exception/enums/StoreReserveErrorResult.java
+++ src/main/java/com/jdh/hoxy_api/api/reserve/exception/enums/StoreReserveErrorResult.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum StoreReserveErrorResult {
 
-    NOT_EXISTS(HttpStatus.BAD_REQUEST, "SR0001", "존재하지 않는 예약 정보입니다."),
+    NOT_EXISTS(HttpStatus.BAD_REQUEST, "SR0001", "이미 처리한 예약 정보입니다."),
     ALREADY_DELETE(HttpStatus.BAD_REQUEST, "SR0002", "이미 삭제된 예약 정보입니다."),
     NOT_THIS_STORE(HttpStatus.BAD_REQUEST, "SR0003", "접근 가능한 예약 정보가 아닙니다. (업체 불일치)");
 
