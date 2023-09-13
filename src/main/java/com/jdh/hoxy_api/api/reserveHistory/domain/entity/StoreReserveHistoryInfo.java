e95d5b99f573a6de024520860904307c23f95219
JangDaeHyeok <jangdaehyeog@gmail.com>
2023-09-13 21:58:46 +0900

    fix: StoreReserveHistoryInfo 등록일자용 어노테이션 수정
    1. @CreateDate -> @CreationTimestamp

--- src/main/java/com/jdh/hoxy_api/api/reserveHistory/domain/entity/StoreReserveHistoryInfo.java
+++ src/main/java/com/jdh/hoxy_api/api/reserveHistory/domain/entity/StoreReserveHistoryInfo.java
@@ -1,6 +1,7 @@
 package com.jdh.hoxy_api.api.reserveHistory.domain.entity;
 
 import com.jdh.hoxy_api.api.reserveHistory.enums.ReserveState;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -8,6 +9,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
@@ -20,7 +22,8 @@ public class StoreReserveHistoryInfo {
     @Enumerated(EnumType.STRING)
     private ReserveState reserveState;
 
-    @CreatedDate
+    @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime reserveProcDt;
 
     @Builder
